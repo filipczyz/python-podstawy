db51540ca243e2b100b51a6143677a08bc24a4c4
filipczyz <filip.czyz@edu.uekat.pl>
2026-01-31 19:59:48 +0100

    api zestawik

--- lab_17_03_2025/zad_4.py
+++
@@ -1,9 +0,0 @@
-def sprawdzanie(jeden:int, dwa:int, trzy:int):
-    return jeden + dwa >= trzy
-
-a = 12
-b = 3
-c = 9
-
-result = sprawdzanie(a, b, c)
-print(result)
--- python_api/main.py
+++ python_api/main.py
@@ -3,77 +3,66 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 class Movie:
     def __init__(self, movie_id, title, genres):
-        self.movie_id = movie_id
+        self.id = movie_id
         self.title = title
         self.genres = genres
 
+class Link:
+    def __init__(self, movie_id, imdb_id, tmdb_id):
+        self.id = movie_id
+        self.imdbId = imdb_id
+        self.tmdbId = tmdb_id
 
+class Rating:
+    def __init__(self, user_id, movie_id, rating, timestamp):
+        self.userId = user_id
+        self.movieId = movie_id
+        self.rating = rating
+        self.timestamp = timestamp
+
+class Tag:
+    def __init__(self, user_id, movie_id, tag, timestamp):
+        self.userId = user_id
+        self.movieId = movie_id
+        self.tag = tag
+        self.timestamp = timestamp
+
+
+def load_data(file_path, model_class):
+    data_list = []
+    try:
+        with open(file_path, mode='r', encoding='utf-8') as file:
+            reader = csv.reader(file)
+            next(reader)
+            for row in reader:
+                obj = model_class(*row)
+                data_list.append(obj.__dict__)
+    except FileNotFoundError:
+        return {"error": f"Plik {file_path} nie został znaleziony."}
+    return data_list
+
+# endpointy
 @app.get("/")
 def read_root():
     return {"hello": "world"}
 
+
 @app.get("/movies")
 def get_movies():
-    movies_list = []
+    return load_data('movies.csv', Movie)
 
 
-    with open('movies.csv', encoding='utf-8') as f:
-        reader = csv.reader(f)
-        next(reader, None)
-
-        for row in reader:
-
-            movie_obj = Movie(movie_id=row[0], title=row[1], genres=row[2])
-
-
-            movies_list.append(movie_obj.__dict__)
-
-    return movies_list
-
-class Tag:
-    def __init__(self, user_id, movie_id, tag, timestamp):
-        self.user_id = user_id
-        self.movie_id = movie_id
-        self.tag = tag
-        self.timestamp = timestamp
-
-@app.get("/tags")
-def get_tags():
-    tags_list = []
-
-    with open('tags.csv', encoding='utf-8') as f:
-        reader = csv.reader(f)
-        next(reader, None)
-
-        for row in reader:
-            tag_obj = Tag(user_id=row[0], movie_id=row[1], tag=row[2], timestamp=row[3])
-            tags_list.append(tag_obj.__dict__)
-
-    return tags_list
-
-
-
-class Rating:
-    def __init__(self, user_id, movie_id, rating, timestamp):
-        self.user_id = user_id
-        self.movie_id = movie_id
-        self.rating = rating
-        self.timestamp = timestamp
-
-
+@app.get("/links")
+def get_links():
+    return load_data('links.csv', Link)
 
 @app.get("/ratings")
 def get_ratings():
-    ratings_list = []
+    return load_data('ratings.csv', Rating)
 
-    with open('ratings.csv', encoding='utf-8') as f:
-        reader = csv.reader(f)
-        next(reader, None)
-
-        for row in reader:
-            rating_obj = Rating(user_id=row[0], movie_id=row[1], rating=row[2], timestamp=row[3])
-            ratings_list.append(rating_obj.__dict__)
-
-    return ratings_list
+@app.get("/tags")
+def get_tags():
+    return load_data('tags.csv', Tag)
